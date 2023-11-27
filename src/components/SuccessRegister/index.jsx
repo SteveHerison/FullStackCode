a9df097b93b861329bc15204438ab71045710419
SteveHerison <steveherisondev@gmail.com>
2023-11-27 12:35:07 -0300

    TESTE

--- src/components/SuccessRegister/index.jsx
+++ src/components/SuccessRegister/index.jsx
@@ -38,7 +38,7 @@ function CustomizedSnackbars() {
       <Alert severity="error">This is an error message!</Alert>
       <Alert severity="warning">This is a warning message!</Alert>
       <Alert severity="info">This is an information message!</Alert>
-      <Alert severity="success">This is a success message!</Alert>
+      <Alert severity="success">This is a </Alert>
     </Stack>
   );
 }
